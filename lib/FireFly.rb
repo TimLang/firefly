e74fa2f676c9c5b1e5f3fe8be001daa03e157dff
Tim <tim@tim-System-Product-Name.(none)>
2013-03-08 14:42:24 +0800

    committing

---
+++ lib/FireFly.rb
@@ -0,0 +1,98 @@
+
+require 'open-uri'
+
+module FireFly
+    VERSION = '0.01'
+
+    def self.start_crawl start_url, options, &block
+        spider = Core.new start_url, options
+        block.call(spider)
+        spider.start
+    end
+
+    module Util
+    end
+
+    module Event
+        def bind
+
+        end
+    end
+
+    module Filter
+    end
+
+    class Core
+        include Util, Event, Filter
+
+        attr_reader :todo_urls, :visited_urls, :filters
+
+        def initialize start_url, options
+            @todo_urls = [start_url]
+            @visited_urls = {}
+            @callbacks = {}
+            @filters = []
+        end
+
+        def start
+            begin
+                base_url = @todo_urls.pop
+                puts "visited: #{base_url}"
+                begin
+                    open(base_url).read.scan(/href=['|"](.+?)['|"]/) do |pending_url|
+                        result_url = get_whole__url base_url, pending_url[0]
+                        @todo_urls << result_url if result_url && !@visited_urls.has_key?(result_url)
+                    end
+                    #puts "todo size is #{@todo_urls.size}"
+                rescue
+                    puts 'error----------------'
+                ensure
+                    @visited_urls[base_url] = nil
+                end
+            end while !@todo_urls.empty?
+        end
+
+        def add_filters &block
+            puts "闭包: #{block}"
+            @filters << block
+        end
+
+        def is_relative_url url
+            /^\// === url
+        end
+
+        def is_correct_url url
+            #/^http/ === url && @filters.map{|f| f.call(url)}.all? 
+            #/^http/ === url && (@filters[0].call(url) unless @filters.empty?)
+            if !@filters.empty?
+                @filters[0].call(url)
+            else
+               /^http/ === url 
+            end
+        end
+
+        def get_whole__url current_url, url
+            if is_correct_url(url)
+                current_url.match(/(http:\/\/([^\/]+))\/?/)[1] << url if is_relative_url(url)     
+                url 
+            end
+        end
+
+        def custom_filter_pattern
+            true
+        end
+
+        def test
+            puts "im Tim Lang"
+        end
+    end    
+end
+
+puts "the version of Firefly is: #{FireFly::VERSION}"
+
+FireFly.start_crawl 'http://www.yesmywine.com.hk', {} do |core|
+    core.test
+    core.add_filters do |url|
+        /www\.yesmywine\.com\.hk/ === url
+    end 
+end
